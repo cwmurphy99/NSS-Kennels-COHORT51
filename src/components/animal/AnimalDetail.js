db53c71d6be7d78f713836f9e4ca9e083966d0e7
Chris Murphy <theworldisyours@gmail.com>
2021-10-02 03:25:00 -0400

    pushing the styling up for preservation

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -4,7 +4,8 @@ import './AnimalDetail.css';
 import { useParams, useHistory } from "react-router-dom"
 
 export const AnimalDetail = () => {
-    const [animal, setAnimal] = useState({ name: "", breed: "" });
+
+    const [animal, setAnimal] = useState({ name: "", breed: "", customer: "", location: "", image: ""});
     const [isLoading, setIsLoading] = useState(true);
     const { animalId } = useParams();
     const history = useHistory();
@@ -23,31 +24,50 @@ export const AnimalDetail = () => {
 
     useEffect(() => {
         //getAnimalById(id) from AnimalManager and hang on to the data; put it into state
-        //console.log("useEffect", animalId)
         getAnimalById(animalId)
             .then(animal => {
                 setAnimal({
                     name: animal.name,
                     breed: animal.breed,
+                    customer: animal.customer.name,
                     location: animal.location.name,
-                    customer: animal.customer.name
+                    image: animal.image
                 });
             });
     }, [animalId]);
 
     return (
-        <section className="animal">
-            <h3 className="animal__name">{animal.name}</h3>
-            <div className="animal__breed">{animal.breed}</div>
-            {/* What's up with the question mark???? See below.*/}
-            <div className="animal__location">Location: {animal.location}</div>
-            <div className="animal__owner">Customer: {animal.customer}</div>
-            <button type="button" disabled={isLoading} onClick={handleDelete}>
-                Discharge
-            </button>
-            <button type="button" onClick={handleBack}>
-                Go Back
-            </button>
-        </section>
+
+        <div className="animal__details__">
+
+            <div className="animal__details__header">
+                {/* <picture>
+                    {animal.image !== "" ?
+
+                        <img src={require(`../../images${animal.image}`).default} alt={animal.name} className="animal__details__photo" />
+
+                        : <p>There isn't an image.</p>}
+                </picture> */}
+            </div>
+
+
+
+            <section className="animal">
+
+                <h3 className="animal__name"> <strong> Name: </strong> {animal.name}</h3>
+
+                <div className="animal__details"> <strong> Breed: </strong> {animal.breed}</div>
+
+                <div className="animal__details"> <strong> Customer: </strong> {animal.customer}</div>
+
+                <div className="animal__details"> <strong> Location: </strong> {animal.location}</div>
+
+                <button className="animal__" type="button" disabled={isLoading} onClick={handleDelete}> Discharge </button>
+
+                <button type="button" onClick={handleBack}> Go Back </button>
+
+            </section>
+
+        </div>
     );
 }
