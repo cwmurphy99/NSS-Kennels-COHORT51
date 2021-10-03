1b8a5c91e3721350b86d5eb359222db24ad48453
Chris Murphy <theworldisyours@gmail.com>
2021-10-02 22:00:29 -0400

    added styles for animal cards and details

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -4,11 +4,22 @@ import { Link } from "react-router-dom"
 
 export const AnimalCard = ({ animal, handleDeleteAnimal }) => (
     <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">Breed: {animal.breed}</div>
-        <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
-        <Link to={`/animals/${animal.id}`}>
-            <button>Details</button>
-        </Link>
+      
+            <div className="animal__content">
+
+                {/* <picture>
+              ${animal.image !== '' ? <img src={require(`../../images${animal.image}`).default} alt={animal.name} className="animal__card__photo" /> : <p>No Image to Display</p>}
+            </picture> */}
+
+                <h3 className="animal__name">{animal.name}</h3>
+
+                <div className="animal__breed">Breed: {animal.breed}</div>
+
+                <button type="button" onClick={() => handleDeleteAnimal(animal.id)}> <strong> Discharge </strong> </button>
+
+                <Link to={`/animals/${animal.id}`}><button> <strong> Details </strong> </button> </Link>
+
+            </div>
+      
     </section>
 )
